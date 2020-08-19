6361ac5bb027516ff6f6c7842e8f342fb25fa495
proctorine <proctorine@gmail.com>
2020-08-19 02:47:25 -0400

    adding the goods

---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+ 
+class NotFound extends Component {
+  render() {
+    return <div>
+      <h4>NotFound</h4>
+      <p>404!<br />Page not found.</p>
+    </div>
+  }
+}
+ 
+export default NotFound;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom'
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NotFound from './NotFound';
+ 
+const routing = (
+  <BrowserRouter>
+    <div>
+      <h3>Clue Mediator (www.cluemediator.com)</h3>
+      
+          <NavLink exact activeClassName="active" to="/">Home</NavLink><br />
+          <NavLink activeClassName="active" to="/about">About</NavLink><br />
+          <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+ 
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+ 
+ReactDOM.render(routing, document.getElementById('root'));
